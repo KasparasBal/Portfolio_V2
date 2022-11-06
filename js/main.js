5d8acb6e460b21c38f6a109fd69f7ad85fd1089d
Kasparas Balys <98013126+KasparasBal@users.noreply.github.com>
2022-11-06 16:34:24 +0200

    finished until other projects

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+"use strict";
+
 //Selections
 const nav = document.querySelector(".nav");
 const heroImage = document.querySelector(".hero__image");
+const heroTitle = document.querySelector(".hero__title");
 
 //Navbar
 
-const handleHover = function (e) {
+const handleLinkHover = function (e) {
   if (e.target.classList.contains("nav__link")) {
     const link = e.target;
     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
@@ -16,5 +19,36 @@ const handleHover = function (e) {
   }
 };
 
-nav.addEventListener("mouseover", handleHover.bind(0.5));
-nav.addEventListener("mouseout", handleHover.bind(1));
+nav.addEventListener("mouseover", handleLinkHover.bind(0.5));
+nav.addEventListener("mouseout", handleLinkHover.bind(1));
+
+//Header Image Hover
+
+heroTitle.addEventListener("mouseover", () => {
+  heroImage.classList.add("bg-opacity");
+});
+heroTitle.addEventListener("mouseout", () => {
+  heroImage.classList.remove("bg-opacity");
+});
+
+//Smooth Scroll
+
+const scrollFunc = function (e) {
+  const link = e.target;
+  document
+    .getElementById(link.getAttribute("href"))
+    .scrollIntoView({ behavior: "smooth" });
+};
+
+nav.addEventListener("click", scrollFunc);
+
+//Sticky Nav
+
+window.addEventListener("scroll", () => {
+  const navHeight = parseInt(window.getComputedStyle(nav).height);
+  if (window.scrollY > `${navHeight}`) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+});
